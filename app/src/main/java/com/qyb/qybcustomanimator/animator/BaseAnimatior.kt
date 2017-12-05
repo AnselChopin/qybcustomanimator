37338abb64c49c7645a7f14c3783554b71b4f0b2
qingyuanbiao <347116198.qq.com>
2017-12-05 16:09:26 +0800

    version 1.0

--- app/src/main/java/com/qyb/qybcustomanimator/animator/BaseAnimatior.kt
+++ app/src/main/java/com/qyb/qybcustomanimator/animator/BaseAnimatior.kt
@@ -4,6 +4,9 @@ package com.qyb.qybcustomanimator.animator
  * Created by Qing Yuanbiao.
  * Created on 2017/12/5.
  * Desc:
+ *      所有自定义动画都实现这个接口，具体实现细节将回调到具体自定义动画中
+ *
+ *
  */
 interface BaseAnimatior {
 
